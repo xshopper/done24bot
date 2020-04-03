59d0bdeef6e37beb33dd3d0ba3c410cb088e26f1
Gergo Bacskai <gbacskai@gmail.com>
2020-04-03 19:05:49 +1100

    no comment if already comment exists from the user

--- index_ig_comment_on_hashtag_posts.js
+++ index_ig_comment_on_hashtag_posts.js
@@ -41,9 +41,10 @@ const ig = {
             await ig.bot.openRecentPostOneByOne(ig.parameters.nr_of_likes, async (col, item) => {
 		//var user = await ig.bot.getUsernameFromPost();
                 await ig.bot.openComments();
-		var myComment = await ig.bot.page.waitFor('//*[@href="/' + ig.bot.username + '"]', { timeout: 100 });
-		if(!myComment)  {
-                await ig.bot.pastComment(ig.parameters.message);
+		try {
+			var myComment = await ig.bot.page.waitFor('//*[@href="/' + ig.bot.username + '"]', { timeout: 100 });
+		} catch(e) {
+                	await ig.bot.pastComment(ig.parameters.message);
 		}
 		//await ig.utils.log({ "comment": ig.parameters.message, "instagram": ig.bot.username , "target_instagram" : user })
 
