8a27f9d1de58b2e0c89e99c9dfd37c034b0f4f61
Gergo Bacskai <gbacskai@gmail.com>
2020-04-17 12:18:22 +1000

    linkedin_in_dummy.js

---
+++ index_in_dummy.js
@@ -0,0 +1,33 @@
+const li = {
+    BASE_URL: 'https://www.linkedin.com',
+    description: 'For Development purpose, opens Linkedin and logs in',
+    window: null,
+    utils: null,
+    bot: null,
+    form: [],
+
+    parameters: null,
+
+    init: async () => {
+        console.log('init...');
+        var module = await li.utils.httpRequestText('https://raw.githubusercontent.com/xshopper/done24bot/master/websites/linkedin.js');
+        li.bot = await li.utils.requireFromString(module);
+        li.bot.utils = li.utils;
+    },
+
+    process: async () => {
+        console.log('process');
+
+        const loginData = await li.bot.login();
+	let log = await li.utils.log({ "filename": "index_in_dummy.js", "function": "process", "url": li.bot.page.url(), "linkedin": li.bot.username });      
+
+        await li.utils.saveCookies(li.bot).catch(function (error) {
+            console.log(error);
+        });
+
+        await li.utils.sleep(100000000);
+
+    },
+}
+
+module.exports = li;
