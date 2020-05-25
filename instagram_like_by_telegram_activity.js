9f79f36dfbfb2dd56941b1ad96adfc5c0382c0ef
Gergo Bacskai <gbacskai@gmail.com>
2020-05-25 10:07:46 +1000

    alert on all links been liked

--- instagram_like_by_telegram_activity.js
+++ instagram_like_by_telegram_activity.js
@@ -5,22 +5,21 @@ video : 'https://drive.google.com/file/d/1zS2AiyuhIs4V_nxwJdkw06GqG5CveBEs/view?
 utils:null,
 parameters:{},
 form: [{ "id": "v_user_id", "elem" : "input", "placeholder" : "user_id from telegram @done24boti (type /start there)"}],
-urls:null,
+data:null,
 init: async() => {
 	console.log('init.......new');
 	ig.bot = await ig.utils.requireFromURL('https://raw.githubusercontent.com/xshopper/done24bot/master/websites/instagram.js')
 	ig.bot.utils = ig.utils;
 
 	console.log('....loading urls');
-	ig.urls = await utils.data ({"method" : "GET", "endpoint" : 'tglist',  "headers" : { "telegram_id" : ig.parameters.v_user_id} })
-	ig.urls = ig.urls.urls;
+	ig.data = await utils.data ({"method" : "GET", "endpoint" : 'tglist',  "headers" : { "telegram_id" : ig.parameters.v_user_id} })
 	console.log('urls:', ig.urls);
 },
 
 like_posts: async () => {
 
-	for (var i = 0; i < ig.urls.length; i++) { /// loop on my posts 
-                await ig.bot.page.goto('https://www.instagram.com/p/' + ig.urls[i] + '/');
+	for (var i = 0; i < ig.data.urls.length; i++) { /// loop on my posts 
+                await ig.bot.page.goto('https://www.instagram.com/p/' + ig.data.urls[i] + '/');
                 
                 var log = await ig.bot.likePost();
                 if (log.wait > 0) {
@@ -38,15 +37,24 @@ process: async () => {
         let log = await ig.utils.log({"filename" : "index_ig_like", "function" : "process", "url" : ig.bot.page.url(), "instagram" : ig.bot.username, "telegram_id" : ig.parameters.v_user_id });
 
 	const loginData = await ig.bot.login();
-	console.log('logged in ...', ig.urls.length)
-	while(ig.urls.length > 0) {
-		console.log('urls:', ig.urls)
+	console.log('logged in ...', ig.data.urls.length)
+
+	if(ig.data.urls.length == 0) {
+		if( ig.data.nr_liked > 0) {
+			return ("Seems all links been liked, check out the admin page for the links and groups");
+		} else {
+			alert("We do not see any of your activity in any groups");
+			return ("please type /group and the invite link at @done24bot in telegram for adding a new group");
+		}
+	}
+
+	while(ig.data.urls.length > 0) {
+		console.log('urls:', ig.data.urls)
 		await ig.utils.saveCookies(ig.bot).catch(function(error) {
             	    console.log(error);
         	});  
 
-		ig.urls = await utils.data ({"method" : "GET", "endpoint" : 'tglist',  "headers" : { "telegram_id" : ig.parameters.v_user_id} })
-        	ig.urls = ig.urls.urls;
+		ig.data.urls = await utils.data ({"method" : "GET", "endpoint" : 'tglist',  "headers" : { "telegram_id" : ig.parameters.v_user_id} })
 		console.log('start loop');
 		await ig.like_posts();
 	}
