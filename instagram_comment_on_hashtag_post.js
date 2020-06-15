cfee77430500b6275a53ef8a9a130f066f9058a0
Gergo Bacskai <gbacskai@gmail.com>
2020-06-15 11:58:49 +1000

    no double commenting

--- instagram_comment_on_hashtag_post.js
+++ instagram_comment_on_hashtag_post.js
@@ -38,22 +38,27 @@ const ig = {
             await ig.bot.navigateToExampleProfile(ig.parameters.hashtag);
 
             await ig.bot.openRecentPostOneByOne(ig.parameters.nr_of_likes, async (col, item) => {
-		//var user = await ig.bot.getUsernameFromPost();
-                await ig.bot.openComments();
-		try {
-			var myComment = await ig.bot.page.waitFor('//*[@role="main"]//*[@href="/' + ig.bot.username + '/"]', { timeout: 2000 });
-		} catch(e) {
-			var myComment  = null;
-		}
+		var instagram_id = await ig.bot.getUsernameFromPost();
+                var logs = await ig.utils.data({ "method": 'GET', "endpoint": 'search/instagram/interaction', "headers": { "instagram_id": instagram_id, "type" : "comment" }})
 
-		if(!myComment) {
-			var log = await ig.bot.pastComment(ig.parameters.message);
-			if (log && log.wait > 0) {
-                                console.log('wait ' + log)
-                                await ig.utils.sleep(log.wait);
-                        }
-		}
+		if (logs.length ===0) {	
+			//var user = await ig.bot.getUsernameFromPost();
+        	        await ig.bot.openComments();
+			try {
+				var myComment = await ig.bot.page.waitFor('//*[@role="main"]//*[@href="/' + ig.bot.username + '/"]', { timeout: 2000 });
+			} catch(e) {
+				var myComment  = null;
+			}
 
+			if(!myComment) {
+	
+				var log = await ig.bot.pastComment(ig.parameters.message);
+				if (log && log.wait > 0) {
+                        	        console.log('wait ' + log)
+                               	 	await ig.utils.sleep(log.wait);
+                        	}
+			}
+		}
                 await ig.bot.goBack();
             });
             return;
