52ba9a2db8487fdcc14da8da70b960d542a6321e
Gergo Bacskai <gbacskai@gmail.com>
2020-06-24 21:16:17 +1000

    logout fix

--- amazon_logout.js
+++ amazon_logout.js
@@ -1,6 +1,6 @@
 const ig = {
 BASE_URL:'https://www.amazon.com.au',
-description: 'Dummy Script for developers, it opens the amazon.com.au website.',
+description: 'Logs out from amazon.com.au',
 window:null,
 utils:null,
 bot : null,
@@ -12,9 +12,6 @@ init: async() => {
 	console.log('init...');
 	ig.bot= await ig.utils.requireFromURL('https://raw.githubusercontent.com/xshopper/done24bot/master/websites/amazon.com.au.js')
 	ig.bot.utils = ig.utils;
-
-	ig.lodash = await ig.utils.requireFromURL('https://raw.githubusercontent.com/lodash/lodash/4.17.15-npm/lodash.js');
-	
 },
 
 process: async () => {
@@ -26,8 +23,6 @@ process: async () => {
 	await ig.utils.saveCookies(ig.bot).catch(function(error) {
 		console.log(error);
 	});
-
-	await ig.utils.sleep(30000000);
 
  }
 }
