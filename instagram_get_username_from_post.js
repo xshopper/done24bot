1bd57333f2fff610ffc6381b1e122a6141c5f05b
Gergo Bacskai <gbacskai@deloitte.com.au>
2020-08-17 22:48:55 +1000

    initial

--- instagram_get_username_from_post.js
+++ instagram_get_username_from_post.js
@@ -1,13 +1,15 @@
 const ig = {
 BASE_URL:null,
-description: 'Just a dummy script for development/testing. It just wait with the login ....',
+description: 'Get Username from Post',
 window:null,
 utils:null,
 bot : null,
 parameters:null,
+form: [{ "id": "post_id", "elem": "input", "placeholder": "CBjjqUVBcy1", "value": "CBjjqUVBcy1" }],
+parameters: null,
 
 init: async() => {
-	ig.BASE_URL = 'https://instagram.com/p/CBjjqUVBcy1/?utm_source=pwa_homescreen';
+	ig.BASE_URL = 'https://instagram.com/p/'+ig.parameters.post_id+'/?utm_source=pwa_homescreen';
 	console.log('init...');
 	ig.bot = await ig.utils.requireFromURL('https://raw.githubusercontent.com/xshopper/done24bot/master/websites/instagram.js')
 	ig.bot.utils = ig.utils;
@@ -15,10 +17,9 @@ init: async() => {
 
 process: async () => {
 	console.log('process');
-        let log = await ig.utils.log({"filename" : "index_ig_dummy", "function" : "process", "url" : ig.bot.page.url(), "instagram" : ig.bot.username });
 	var username = await ig.bot.getUsernameFromPost();
-	console.log(username);
-	return true;
+	console.log("#post_id#", ig.parameters.post_id, "#username#",username);
+	return username;
 }
 }
 
