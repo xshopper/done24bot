25375c8a1789bede5de336e2c4d75731cf62e0da
Gergo Bacskai <gbacskai@deloitte.com.au>
2020-08-17 21:49:36 +1000

    get username_test

---
+++ instagram_get_username_from_post.js
@@ -0,0 +1,31 @@
+const ig = {
+BASE_URL:null,
+description: 'Just a dummy script for development/testing. It just wait with the login ....',
+window:null,
+utils:null,
+bot : null,
+parameters:null,
+
+init: async() => {
+	ig.BASE_URL = 'https://instagram.com/p/CBjjqUVBcy1/?utm_source=pwa_homescreen';
+	console.log('init...');
+	ig.bot = await ig.utils.requireFromURL('https://raw.githubusercontent.com/xshopper/done24bot/master/websites/instagram.js')
+	ig.bot.utils = ig.utils;
+},
+
+process: async () => {
+	console.log('process');
+        let log = await ig.utils.log({"filename" : "index_ig_dummy", "function" : "process", "url" : ig.bot.page.url(), "instagram" : ig.bot.username });
+
+	const loginData = await ig.bot.login();
+
+	await ig.utils.saveCookies(ig.bot).catch(function(error) {
+		console.log(error);
+	});
+	var username = await ig.bot.getUsernameFromPost();
+	console.log(username);
+	return true;
+}
+}
+
+module.exports = ig;
