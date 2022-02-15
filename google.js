66d01a5ab6a7e1fae78c2deb51832f9215f166a2
Gergo Bacskai <16809418+gbacskai@users.noreply.github.com>
2022-02-15 16:11:53 +1100

    Update google.js

--- google.js
+++ google.js
@@ -1,5 +1,14 @@
-async src="https://www.googletagmanager.com/gtag/js?id=G-7XQXK9Z1MW"
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-7XQXK9Z1MW');
+
+(function() {
+  
+var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
+po.src = 'https://www.googletagmanager.com/gtag/js?id=G-7XQXK9Z1MW';
+var s = document.getElementsByTagName('script')[0]; 
+s.parentNode.insertBefore(po, s);
+
+var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = false;
+po.src = "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());gtag('config', 'G-7XQXK9Z1MW');';"
+var s = document.getElementsByTagName('script')[0]; 
+s.parentNode.insertBefore(po, s);
+
+})();
