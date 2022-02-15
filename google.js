bdc2a3a1b01f4f0389e8b4c201a82ebad53bd130
Gergo Bacskai <16809418+gbacskai@users.noreply.github.com>
2022-02-15 15:57:32 +1100

    Update google.js

--- google.js
+++ google.js
@@ -1,9 +1,5 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-7XQXK9Z1MW"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-7XQXK9Z1MW');
-</script>
+async src="https://www.googletagmanager.com/gtag/js?id=G-7XQXK9Z1MW"
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-7XQXK9Z1MW');
