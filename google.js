0adb49c802e90daf75fb21a7ca7d872773209b76
Gergo Bacskai <16809418+gbacskai@users.noreply.github.com>
2022-02-15 16:15:55 +1100

    Update google.js

--- google.js
+++ google.js
@@ -7,7 +7,7 @@ var s = document.getElementsByTagName('script')[0];
 s.parentNode.insertBefore(po, s);
 
 po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = false;
-po.src = "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());gtag('config', 'G-7XQXK9Z1MW');';"
+po.text = "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());gtag('config', 'G-7XQXK9Z1MW');';"
 s = document.getElementsByTagName('script')[0]; 
 s.parentNode.insertBefore(po, s);
 
